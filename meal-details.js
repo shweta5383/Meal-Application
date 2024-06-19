ad2d57674a9e08996ec0322a2dc66dbf96ecb646
shweta5383 <57386933+shweta5383@users.noreply.github.com>
2024-06-19 10:24:45 +0530

    Update meal-details.js

--- meal-details.js
+++ meal-details.js
@@ -1,16 +1,15 @@
+//Display Meal details
+const mealName = document.getElementById('meal-name');
+const mealImg = document.getElementById('meal-image');
+const mealInstruction = document.getElementById('meal-instructions');
 const apiUrl = 'https://www.themealdb.com/api/json/v1/1/';
 const mealId = new URLSearchParams(window.location.search).get('id');
 fetch(`${apiUrl}lookup.php?i=${mealId}`)
     .then(response => response.json())
     .then(data => {
         const meal = data.meals[0];
-        console.log(meal);
-        const mealDetailElement = document.getElementsByClassName('meal-detail-container');
-        const meal_name = document.getElementById('meal-name');
-        const meal_img = document.getElementById('meal-image');
-        const meal_instructions = document.getElementById('meal-instructions');
-        meal_name.textContent = meal.strMeal;
-        meal_img.src = meal.strMealThumb;
-        meal_instructions.textContent = meal.strInstructions;
+        mealName.textContent = meal.strMeal;
+        mealImg.src = meal.strMealThumb;
+        mealInstruction.textContent = meal.strInstructions;
     })
     .catch(error => console.error(error));
